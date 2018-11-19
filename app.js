56080330b83684b8169847f473b37cf122d64fd9
Kenny Stephens <10468762@uvu.edu>
2018-11-19 12:16:25 -0700

    map Array Helper

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 // Array Helpers
 
-// forEach ---------------------------------------------------------
-var colors = ['red', 'blue', 'green'];
+// forEach --------------------------------------------------------->
+// var colors = ['red', 'blue', 'green'];
 
-// for(var i = 0; i < colors.length; i++) {
-//     console.log(colors[i]);
+// // for(var i = 0; i < colors.length; i++) {
+// //     console.log(colors[i]);
+// // }
+
+// // colors.forEach(color => console.log(color));
+
+// // Create an array of numbers
+// var numbers = [1,2,3,4,5];
+
+// // Create a variable to hold the sum
+// var sum = 0;
+
+// // Loop over the array, incrementing the sum variable
+// numbers.forEach(number => {
+//     sum += number;
+// });
+
+// // Print the sum variable
+// console.log(sum);
+
+// map ----------------------------------------------------------->
+var numbers = [1,2,3];
+var doubledNumbers = [];
+
+// for(var i = 0; i < numbers.length; i++) {
+//     doubledNumbers.push(numbers[i] * 2);
 // }
 
-// colors.forEach(color => console.log(color));
+var doubled = numbers.map(number => number * 2);
 
-// Create an array of numbers
-var numbers = [1,2,3,4,5];
+console.log(doubledNumbers);
+console.log(doubled);
 
-// Create a variable to hold the sum
-var sum = 0;
+var cars = [
+    { model: 'Buick', price: 'CHEAP' },
+    { model: 'Camaro', price: 'EXPENSIVE' }
+];
 
-// Loop over the array, incrementing the sum variable
-numbers.forEach(number => {
-    sum += number;
-});
+var prices = cars.map(car => car.price);
 
-// Print the sum variable
-console.log(sum);
+console.log(prices);
