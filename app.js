313ba4734cfe51788da08eb451406f24ff677191
Kenny Stephens <10468762@uvu.edu>
2018-11-26 08:21:22 -0700

    Learned find Array Helper

--- app.js
+++ app.js
@@ -72,29 +72,81 @@ var products = [
 
 // Type is 'vegetable', quanitity is greater than 0, price is less than 10
 
-let advancedFilter = products.filter(product => {
-    return product.type === 'vegetable' && product.quantity > 0 && product.price < 10;
-});
+// let advancedFilter = products.filter(product => {
+//     return product.type === 'vegetable' && product.quantity > 0 && product.price < 10;
+// });
 
-// console.log(advancedFilter);
+// // console.log(advancedFilter);
 
-var post = { id: 4, title: 'New Post'};
+// var post = { id: 4, title: 'New Post'};
 
-var comments = [
-    { postId: 4, content: 'awesome post' },
-    { postId: 3, content: 'it was ok' },
-    { postId: 4, content: 'neat' },
-];
+// var comments = [
+//     { postId: 4, content: 'awesome post' },
+//     { postId: 3, content: 'it was ok' },
+//     { postId: 4, content: 'neat' },
+// ];
 
-function commentsForPost(post, comments) {
-    return comments.filter(comment => {
-        return comment.postId === post.id;
-    })
-};
+// function commentsForPost(post, comments) {
+//     return comments.filter(comment => {
+//         return comment.postId === post.id;
+//     })
+// };
 
-console.log(commentsForPost(post, comments));
+// console.log(commentsForPost(post, comments));
+
+// Find ----------------------------------------------------------->
+
+// var users = [
+//     { name: 'Jill' },
+//     { name: 'Alex' },
+//     { name: 'Bill' },
+// ];
+
+// var user;
+
+// // for (var i = 0; i < users.length; i++) {
+// //     if (users[i].name === 'Alex') {
+// //         user = users[i];
+// //         break;
+// //     }
+// // }
 
 
 
+// const test = users.find(user => {
+//     return user.name ==='Alex';
+// });
 
+// console.log(test);
+
+// function Car(model) {
+//     this.model = model;
+// }
+
+// var cars = [
+//     new Car('Buick'),
+//     new Car('Camaro'),
+//     new Car('Focus')
+// ];
+
+// const selectedCar = cars.find(car => {
+//     return car.model === 'Focus';
+// });
+
+// console.log(selectedCar);
+
+// var posts = [
+//     { id: 1, title: 'New Post' },
+//     { id: 2, title: 'Old Post' },
+// ];
+
+// var comment = { postId: 1, content: 'Great Post'};
+
+// function postForComment(posts, comment) {
+//     return posts.find((post) => {
+//         return post.id === comment.postId;
+//     });
+// };
+
+// console.log(postForComment(posts, comment));
 
